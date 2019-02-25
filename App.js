03ff460cad1898c377833113eb9e6171d4a00588
dendanish <45380962+kgaedhwujan@users.noreply.github.com>
2019-02-25 18:23:22 +0800

    Update App.js

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Muhammad Danish Fitri Bin Armas 1615551
+        <Text>Tuto 2nd class
         </Text>
       </View>
     );
